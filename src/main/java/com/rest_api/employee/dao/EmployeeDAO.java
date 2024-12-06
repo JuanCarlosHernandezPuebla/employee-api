8fd155b51094e105261156e1489e4ddcce131815
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2024-12-05 23:23:32 -0600

    Add CRUD endpoints for employee api

---
+++ src/main/java/com/rest_api/employee/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.rest_api.employee.dao;
+
+import com.rest_api.employee.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
